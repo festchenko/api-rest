7ff9a8d068ce1bbf91b61482f9345b664675d691
festchenko <festchenko.d@gmail.com>
2020-02-24 15:23:29 +0200

    fixed path to jsons

--- src/test/java/providers/WeatherstackDataProvider.java
+++ src/test/java/providers/WeatherstackDataProvider.java
@@ -5,6 +5,7 @@ import utils.Properties;
 
 
 public class WeatherstackDataProvider {
+    public static final String DIR = System.getProperty("user.dir");
     public static String APPIACCESSKEY = Properties.INSTANCE.readProperties("weatherstack.apiAccessKey", "/src/test/java/resources/properties/weatherstackApi.properties");
     public static String BASEURL =  Properties.INSTANCE.readProperties("weatherstack.baseUrl", "/src/test/java/resources/properties/weatherstackApi.properties");
     public static String CURRENTWEATHER =  Properties.INSTANCE.readProperties("weatherstack.currentWeather", "/src/test/java/resources/properties/weatherstackApi.properties");
@@ -13,12 +14,12 @@ public class WeatherstackDataProvider {
     @DataProvider(name = "citiesUA")
     public static Object[][] getCitiesUA()
     {
-        return new Object[][] { { "Cherkasy","/Users/festchenko/IdeaProjects/api-rest/src/test/java/resources/json/locationCherkassy.json" }, { "Kiev","/Users/festchenko/IdeaProjects/api-rest/src/test/java/resources/json/locationKiev.json" }, { "Vinnitsa" ,"/Users/festchenko/IdeaProjects/api-rest/src/test/java/resources/json/locationVinnitsa.json" } };
+        return new Object[][] { { "Cherkasy", DIR + "/src/test/java/resources/json/locationCherkassy.json" }, { "Kiev", DIR + "/src/test/java/resources/json/locationKiev.json" }, { "Vinnitsa" , DIR + "/src/test/java/resources/json/locationVinnitsa.json" } };
     }
 
     @DataProvider(name = "citiesUSA")
     public static Object[][] getCitiesUSA()
     {
-        return new Object[][] { { "New York","/Users/festchenko/IdeaProjects/api-rest/src/test/java/resources/json/locationNewYork.json" }, { "Los Angeles","/Users/festchenko/IdeaProjects/api-rest/src/test/java/resources/json/locationLosAngeles.json" }, { "San Francisco","/Users/festchenko/IdeaProjects/api-rest/src/test/java/resources/json/locationSanFrancisco.json" } };
+        return new Object[][] { { "New York",DIR + "/src/test/java/resources/json/locationNewYork.json" }, { "Los Angeles", DIR + "/src/test/java/resources/json/locationLosAngeles.json" }, { "San Francisco", DIR + "/src/test/java/resources/json/locationSanFrancisco.json" } };
     }
 }
